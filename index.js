770dbca851ef2f1d1d6ce51f2af7d6d4d0f9b3f9
Santiago Mozo <smozo615@gmail.com>
2022-05-17 23:41:49 -0500

    building our Array

---
+++ index.js
@@ -0,0 +1,16 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length; // Arrays in JS return the Array's length when "push" is used.
+  }
+}
+
+const myArray = new MyArray();
