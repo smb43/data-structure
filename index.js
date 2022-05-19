0b5585376604d79922408ae6c6dceb2524c7725b
Santiago Mozo <smozo615@gmail.com>
2022-05-18 23:15:33 -0500

    challenge: shift and unshift

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ class MyArray {
     this.length = 0;
     this.data = {};
   }
-  get(index) {
+  getItemAtPosition(index) {
     return this.data[index];
   }
+  // Add Item To The End
   push(item) {
     this.data[this.length] = item;
     this.length++;
     return this.length; // Arrays in JS returns the Array's length when "push" is used.
+  }
+  // Add Item To The Beginning
+  unShift(item) {
+    for(let i = this.length; i >= 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.length;
   }
   // Shift item's index
   shiftItem(index) {
@@ -24,11 +34,17 @@ class MyArray {
     this.length--;
     return item;
   }
-  // remove the last element at the "Array"
+  // Remove Last Item
   pop() {
     const lastItem = this.data[this.length - 1];
     this.removeItemAtPosition(this.length - 1);
     return lastItem;
+  }
+  // Remove First Item
+  shift() {
+    const firstItem = this.data[0];
+    this.removeItemAtPosition(0);
+    return firstItem;
   }
 }
 
