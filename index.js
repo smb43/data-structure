c124d9b080e866058e6cf63ab48f314a321e8d75
Santiago Mozo <smozo615@gmail.com>
2022-05-25 01:11:11 -0500

    Linked List: SLL insert method

--- index.js
+++ index.js
@@ -28,6 +28,34 @@ class MySinglyLinkedList {
     this.length++;
     return this;
   }
+  insert({ value, index }) {
+    if (index >= this.length) {
+      console.log(
+        "the index is equal to or greater than the length of the linked list, so we'll use the append method"
+      );
+      return this.append(value);
+    }
+    if (index < 1) {
+      console.log("the index is lower than 1, so we'll use the prepend method");
+      return this.prepend(value);
+    }
+    const newNode = new Node(value);
+    const prevNode = this.getNode(index - 1);
+    const nextNode = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+    this.length++;
+    return this;
+  }
+  getNode(index) {
+    const counter = 0;
+    const currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 let mySinglylinkedList = new MySinglyLinkedList(1);
