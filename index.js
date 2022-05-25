d7aa3239f32dca61023a11fb27a5ceeec9a46f05
Santiago Mozo <smozo615@gmail.com>
2022-05-24 23:18:54 -0500

    Linked List: building a Singly linked list

--- index.js
+++ index.js
@@ -1,57 +1,19 @@
-class HashTable {
-  constructor(size) {
-    this.data = new Array(size);
-  }
-  hashMethod(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
-    }
-    return hash;
-  }
-  set(key, value) {
-    const address = this.hashMethod(key);
-    if (!this.data[address]) {
-      this.data[address] = [];
-    }
-    this.data[address].push([key, value]);
-    return this.data;
-  }
-  get(key) {
-    const address = this.hashMethod(key);
-    const currentBucket = this.data[address];
-    if (currentBucket) {
-      for (let i = 0; i < currentBucket.length; i++) {
-        if (currentBucket[i][0] === key) return currentBucket[i][1];
-      }
-    }
-    return `El elemento ${key} no existe`;
-  }
-  delete(key) {
-    const address = this.hashMethod(key);
-    const currentBucket = this.data[address];
-    if (currentBucket) {
-      for (let i = 0; i < currentBucket.length; i++) {
-        if (currentBucket[i][0] === key) {
-          currentBucket.splice(i, 1);
-          return this.data;
-        }
-      }
-    }
-    return `El elemento ${key} no existe en esta Hash Table`;
-  }
-  getAllKeys() {
-    let keys = [];
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        this.data[i].map((currentBucket) => keys.push(currentBucket[0]));
-      }
-    }
-    if (keys) {
-      keys = `En este momento NO hay elementos dentro de la Hash Table`
-    }
-    return keys;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-const myHashTable = new HashTable(50);
+class MySinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let mySinglylinkedList = new MySinglyLinkedList(1);
