ee661625aa01b7325e3870e05b78ac7c51294723
Santiago Mozo <smozo615@gmail.com>
2022-05-26 23:51:37 -0500

    Linked List: DLL insert method

--- index.js
+++ index.js
@@ -32,6 +32,38 @@ class MyDoublyLinkedList {
     this.length++;
     return this;
   }
+  getNode(index) {
+    let position = 0;
+    let currentNode = this.head;
+    while (position <= this.length) {
+      if (position === index) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+      position++;
+    }
+  }
+  insert({ value, index }) {
+    if (index >= this.length) {
+      console.log(
+        `the index is equal to or greater than the length. we'll use append method`
+      );
+      return this.append(value);
+    }
+    if (index < 1) {
+      console.log(`the index is lower than 1. we'll use prepend method`);
+      return this.prepend(value);
+    }
+    const newNode = new Node(value);
+    const prevNode = this.getNode(index - 1);
+    const nextNode = prevNode.next;
+    prevNode.next = newNode;
+    newNode.prev = prevNode;
+    nextNode.prev = newNode;
+    newNode.next = nextNode;
+    this.length++;
+    return this;
+  }
 }
 
 const myDoublyLinkedList = new MyDoublyLinkedList(1);
